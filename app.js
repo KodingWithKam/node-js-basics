3ec42ad91ab20a5995e4ee6b9d1378a1a66d4fb0
khayes <khayes@hothouseinc.com>
2020-11-14 10:33:07 -0500

    routing

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ const http = require('http');
 // Create node server
 const server = http.createServer((req, res) => {
     // read request data
-    console.log(req.url, req.method, req.headers);
+    // console.log(req.url, req.method, req.headers);
+
+    const url = req.url;
+
+    if (url === '/') {
+        res.write(`
+            <html lang="en">
+                <head><title>Enter Message</title></head>
+                <body><form action="/message" method="post"><input type="text" name="message"><button type="submit">Send</button></form></body>
+            </html>
+        `);
+        return res.end();
+    }
 
     // Send response (set header)
     res.setHeader('Content-Type', 'text/html');
