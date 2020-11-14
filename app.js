af6406636688cc8d5f572ffaed0f5c2d65426874
khayes <khayes@hothouseinc.com>
2020-11-14 10:24:49 -0500

    sending responses

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const http = require('http');
 
 // Create node server
 const server = http.createServer((req, res) => {
+    // read request data
     console.log(req.url, req.method, req.headers);
+
+    // Send response (set header)
+    res.setHeader('Content-Type', 'text/html');
+    // Set response to be simple html page
+    res.write(`
+        <html lang="en">
+        <head><title>My First Node App</title></head>
+        <body><h1>Hello from my Node server!</h1></body>
+        </html>
+    `);
+    res.end();
 });
 
 server.listen(3000);
